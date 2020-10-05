28fc591b9a7e1c63a2cd0991df5bc2b9dd1f4703
Jonas <jonas@anova.be>
2020-10-05 21:40:37 +0200

    added customer attribute

--- api/src/main/scala/io/guanaco/alerta/api/Alert.scala
+++ api/src/main/scala/io/guanaco/alerta/api/Alert.scala
@@ -13,24 +13,28 @@ case class Alert(
     value: Option[String] = None,
     correlate: Option[Seq[String]] = None,
     timeout: Long = 7 * 24 * 60 * 60 /* 7 days */,
-    attributes: Map[String, String] = Map.empty
+    attributes: Map[String, String] = Map.empty,
+    customer: Option[String] = None
 ) {
 
   def withText(text: String): Alert =
-    Alert(resource, event, service, environment, severity, Some(text), value, correlate, timeout, attributes)
+    Alert(resource, event, service, environment, severity, Some(text), value, correlate, timeout, attributes, customer)
 
   def withSeverity(severity: String): Alert =
-    Alert(resource, event, service, environment, severity, text, value, correlate, timeout, attributes)
+    Alert(resource, event, service, environment, severity, text, value, correlate, timeout, attributes, customer)
 
   def withValue(value: String): Alert =
-    Alert(resource, event, service, environment, severity, text, Some(value), correlate, timeout, attributes)
+    Alert(resource, event, service, environment, severity, text, Some(value), correlate, timeout, attributes, customer)
 
   def withEnvironment(environment: String): Alert =
-    Alert(resource, event, service, Some(environment), severity, text, value, correlate, timeout, attributes)
+    Alert(resource, event, service, Some(environment), severity, text, value, correlate, timeout, attributes, customer)
 
   def withAttributes(attrs: Map[String, String]): Alert =
-    Alert(resource, event, service, environment, severity, text, value, correlate, timeout, attributes ++ attrs)
+    Alert(resource, event, service, environment, severity, text, value, correlate, timeout, attributes ++ attrs, customer)
 
   def withAttribute(attribute: (String, String)): Alert =
-    Alert(resource, event, service, environment, severity, text, value, correlate, timeout, attributes + attribute)
+    Alert(resource, event, service, environment, severity, text, value, correlate, timeout, attributes + attribute, customer)
+
+  def withCustomer(customer: String): Alert =
+    Alert(resource, event, service, environment, severity, text, value, correlate, timeout, attributes, Some(customer))
 }
